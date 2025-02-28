9eb030738658f44e6b4a7068d43577d0e5b240b5
pokosure <hugh.takahashi@icloud.com>
2025-02-28 01:56:34 -0800

    .

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -2,31 +2,35 @@ package org.firstinspires.ftc.teamcode;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
-import dev.frozenmilk.util.units.*;
-import dev.frozenmilk.util.*;
+import dev.frozenmilk.util.units.distance.Distance;
+import dev.frozenmilk.util.units.distance.DistanceUnits;
+import dev.frozenmilk.util.units.distance.Distances;
+
 import java.lang.Math;
 
 
 public class Constants{
-    // Drivetrain Dimensions
+    // Physical Constants
         //length
-    Distance distance =
-    double kTrackWidth = 12;
-        //width
-    double kWheelDiameter = 45;
-    double kWheelCircumference = kWheelDiameter*Math.PI;
+    Distance kWheelBase = new Distance(DistanceUnits.INCH, 10.0);
+    Distance kTrackWidth = new Distance(DistanceUnits.INCH, 10.0);
+    Distance kWheelDiameter = new Distance(DistanceUnits.INCH, 2.0);
+
     // Mechanical/Power Settings
-    double powerPercentage = 1;
+    double powerPercentage = 1.0;
+    double motorrpm = 5900.0;
     double kdrivegearratio = 6.93333333333;
-    double ksteergearratio = 1;
+    double ksteergearratio = 1.0;
     //
 
 
 
 
     //Dependent
+    //Physical Constants
+    Distance kWheelCircumference = kWheelDiameter.times(Math.PI);
     //Mechanical/Power Setting
-    double kMaxAngularSpeedRadiansPerSecond = 5900/drivegearratio/60*2*Math.PI;
+    double kMaxAngularSpeedRadiansPerSecond = motorrpm/kdrivegearratio/60.0
 
 
 
